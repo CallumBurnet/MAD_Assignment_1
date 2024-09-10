1e4b51d5de20ba10d1f09ddf2ac14a3b35ac22d3
Callum <21473492@student.curtin.edu.au>
2024-09-10 08:14:20 +0800

    Reverted back to the 2 viewmodel

--- app/src/main/java/com/example/mad_assignment_1/GameActivity.kt
+++ app/src/main/java/com/example/mad_assignment_1/GameActivity.kt
@@ -11,6 +11,7 @@ class GameActivity : AppCompatActivity() {
     companion object {
         const val GRID_ROWS = "com.game_activity.grid_rows"
         const val GRID_COLS = "com.game_activity.grid_cols"
+        const val IS_SINGLE_PLAYER = "com.game_activity.is_single_player"
     }
     private val gameViewModel: GameInformationModel by viewModels()
     private lateinit var binding: GameBinding
@@ -21,17 +22,19 @@ class GameActivity : AppCompatActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        gameViewModel.isSinglePlayer.value = false //FOR TESTING PURPOSES
 
         if (savedInstanceState == null) {
             Log.d("GameActivity", "Found Grid rows and cols")
             numRows = intent.getIntExtra(GameActivity.GRID_ROWS,  7)
             numCols = intent.getIntExtra(GameActivity.GRID_COLS, 6)
+            gameViewModel.isSinglePlayer.value = intent.getBooleanExtra(GameActivity.IS_SINGLE_PLAYER, false)
+
         }
 
         super.onCreate(savedInstanceState)
         binding = GameBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         cells.addAll(List(numRows * numCols) {index ->
 
             val row  = index/numCols //logic for row of cell
@@ -42,8 +45,9 @@ class GameActivity : AppCompatActivity() {
             if(cell.player == 1 ||  cell.player == 2){
                 return@CellAdapter //returns
             }
+            print(gameViewModel.isSinglePlayer.value == false)
             if(gameViewModel.isSinglePlayer.value == false){ //checks if gamemode is single player
-                if(gameViewModel.currentTurn.value == 0){
+                if(gameViewModel.currentTurn.value == 2){
                     // current turn value 0 is AI or player 2
                     cell.player = 2
                 }else{
@@ -51,16 +55,18 @@ class GameActivity : AppCompatActivity() {
                 }
 
             }else{ //Only 1 player vs AI
-                if(gameViewModel.currentTurn.value == 0){
+                println("MADE IT")
+                if(gameViewModel.currentTurn.value == 2){
                     return@CellAdapter //return if its not your turn to click ~ shouldnt happen
                 }else{
                     cell.player = 1; //Player 1's turn played
                 }
             }
 
-            gameViewModel.updateBoard() //updates the turn value
+            gameViewModel.updateBoard(cells, numRows, numCols) //updates the turn value
 
         }
+
         binding.recyclerView.layoutManager = GridLayoutManager(this,numCols ) //sets up the grid
         binding.recyclerView.adapter = adapter
 
--- app/src/main/java/com/example/mad_assignment_1/MenuInformationModel.kt
+++ app/src/main/java/com/example/mad_assignment_1/MenuInformationModel.kt
@@ -8,9 +8,16 @@ class MenuInformationModel : ViewModel() {
     private val _gridSize = MutableLiveData<GridSize>()
     val gridSize: LiveData<GridSize> get() = _gridSize
 
+    private val _isSinglePlayer = MutableLiveData<Boolean>()
+    val isSinglePlayer: LiveData<Boolean> get() = _isSinglePlayer
+
     fun setGridSize(gridSize: GridSize) {
         _gridSize.value = gridSize
     }
+    fun setGameMode(isSinglePlayer: Boolean){
+        _isSinglePlayer.value = isSinglePlayer
+    }
+
 
     enum class GridSize {
         SMALL,

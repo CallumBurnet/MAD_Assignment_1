1cec40f107bd453f8a0d2f1dc234f068d2023407
Callum <21473492@student.curtin.edu.au>
2024-09-20 10:42:57 +0800

    Cleaned the merge up

--- app/src/main/java/com/example/mad_assignment_1/Entities.kt
+++ app/src/main/java/com/example/mad_assignment_1/Entities.kt
@@ -2,6 +2,7 @@ package com.example.mad_assignment_1
 
 import android.os.Parcel
 import android.os.Parcelable
+
 import androidx.room.Embedded
 import androidx.room.Entity
 import androidx.room.ForeignKey
